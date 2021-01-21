dfa6fb917d24e9f24f072fd9133afb8a09ca9aac
Jean M S A Santos <jeaanmichel@protonmail.com>
2021-01-20 23:08:09 -0300

    signup screen stylized

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,28 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button} from 'react-native-elements';
 
 import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <>
+    <View style={styles.container}>
       <Spacer>
         <Text h3>Sign Up for Tracker</Text>
       </Spacer>
       <Spacer>
-        <Input label="Email" />
-        <Input label="Password" />
+        <Input 
+          label="Email" 
+          value={email} 
+          onChange={setEmail} 
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Input 
+          secureTextEntry
+          label="Password" 
+          value={password}
+          onChange={setPassword}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
       </Spacer>
       <Spacer>
         <Button 
           title="Sign Up"
         />
       </Spacer>
-    </>
+    </View>
   );
+};
+
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 200,
+  }
+});
 
 export default SignupScreen;
